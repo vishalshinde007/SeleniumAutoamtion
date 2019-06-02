62c812b60a66b46404d1b11852a8a3964fa9d20f
vishal <vishalshinde0007@gmail.com>
2019-06-02 12:18:57 +0530

    commit

---
+++ BasicCoreJava/src/loop/Program2.java
@@ -0,0 +1,47 @@
+package loop;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i,j,k,l;
+		
+		for (i=1 ; i<=4;i++)
+		{
+			System.out.println(i + " ");
+				
+			for (j=1 ; j<=i; j++)
+			{
+				
+				if (j==3)
+				{
+					break;
+					
+				}
+				else
+				{
+				System.out.print(j + " ");
+					
+				}	
+				
+				
+			}
+			
+			
+					
+			
+			
+			
+			
+		}
+				
+				
+			}
+			
+			
+		
+
+	
+
+}
