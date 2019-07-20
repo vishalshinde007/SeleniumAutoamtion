7cdebd3b685b9e107495c7d13de42db9dbd40b33
vishal <vishalshinde0007@gmail.com>
2019-07-20 13:10:18 +0530

    commitTestNG

--- TestNGFramework/src/logicTest/TestNG1.java
+++ TestNGFramework/src/logicTest/TestNG1.java
@@ -1,10 +1,29 @@
 package logicTest;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TestNG1 {
 	
-	@Test
+	
+	
+	@Test(priority=1)
+	public void test2()
+	{
+		
+		System.out.println("Test 2");
+		
+		Assert.assertEquals("Anand","nand");
+	}
+	
+	@Test(priority=0)
+	public void test3()
+	{
+		
+		System.out.println("Test 3");
+	}
+	
+	@Test(priority=2)
 	public void test1()
 	{
 		
